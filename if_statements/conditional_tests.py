6d676b6259b80dd5478fbd6badde56ce6e761c01
Nandan <193489667+nandano@users.noreply.github.com>
2025-02-28 21:57:15 +0530

    Solve conditional test exercise

---
+++ if_statements/conditional_tests.py
@@ -0,0 +1,19 @@
+car = 'suzuki'
+print("Is car == 'suzuki'? I predict True")
+print(car == 'suzuki')
+
+print("\nIs car == 'skoda'? I predict False")
+print(car == 'skoda')
+
+print("\nIs car == 'honda'? I predict False")
+print(car == 'honda')
+
+subject = 'mathematics'
+print("\nIs subject == 'physics'? I predict False")
+print(subject == 'physics')
+
+print("\nIs subject == 'math'? I predict False")
+print(subject == 'math')
+
+print("\nIs subject == 'mathematics'? I predict True")
+print(subject == 'mathematics')
