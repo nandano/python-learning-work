a52c7099d94317d2d3692bd64d3ac24c2edffebd
Nandan <193489667+nandano@users.noreply.github.com>
2025-03-01 20:41:15 +0530

    Complete if statements and lists

--- if_statements/toppings.py
+++ if_statements/toppings.py
@@ -1,10 +1,12 @@
-requested_toppings = ['mushrooms', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                      'pepperoni', 'pinapple', 'extra cheese']
 
-if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
 
 print("\nFinished making your pizza!")
