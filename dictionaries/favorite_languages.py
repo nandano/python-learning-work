a8bda0959514f89eff2d5f9f4cb82d25462d8ea8
Nandan <193489667+nandano@users.noreply.github.com>
2025-03-03 22:01:43 +0530

    Create user.py and learn looping through a dictionary

--- dictionaries/favorite_languages.py
+++ dictionaries/favorite_languages.py
@@ -5,5 +5,6 @@ favorite_languages = {
     'phil': 'python',
     }
 
-language = favorite_languages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+print("The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
