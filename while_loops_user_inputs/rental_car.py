2f3ccf1ede9e4810994a59d6b5b3bbce2ad87df6
Nandan <193489667+nandano@users.noreply.github.com>
2025-03-05 14:15:33 +0530

    Solve exercise on user input

---
+++ while_loops_user_inputs/rental_car.py
@@ -0,0 +1,2 @@
+rental_car = input("What kind of rental car would you like? ")
+print(f"Let me check if I can find you a {rental_car.title()}.")
---
+++ while_loops_user_inputs/restaurant_seating.py
@@ -0,0 +1,7 @@
+people_no = input("How many people are in your dinner group? ")
+people_no = int(people_no)
+
+if people_no > 8:
+    print("\nYou'll have to wait for a table.")
+else:
+    print("\nYour table is ready.")
