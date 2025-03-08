bc9c2b2275d9e6fa21cafe7ca98750c04bdd1427
Nandan <193489667+nandano@users.noreply.github.com>
2025-03-08 20:29:11 +0530

    Solve passing a list to a function exercise

---
+++ functions/archived_messages.py
@@ -0,0 +1,13 @@
+def send_messages(messages, sent_messages):
+    while messages:
+        current_message = messages.pop()
+        print(f"Sending: {current_message}")
+        sent_messages.append(current_message)
+
+messages = ["hey there", "good morning", "Hello, how are you?", "I'm fine"]
+empty_list = []
+
+send_messages(messages[:], empty_list)
+
+print(messages)
+print(empty_list)
